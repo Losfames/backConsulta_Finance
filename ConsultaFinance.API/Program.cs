b29038cc4064d307633974389e20cc9a2f6cfbb9
losfames <felipep21silva@gmail.com>
2026-09-10 12:42:40 -0300

    Corrige CORS e URL da API

--- ConsultaFinance.API/Program.cs
+++ ConsultaFinance.API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("PermitirReact", policy =>
     {
         policy.WithOrigins("http://localhost:3000",
-        "https://front-consulta-finance.vercel.app"
+        "https://front-consulta-finance-alm21.vercel.app"
         ) // URL exata do seu React
               .AllowAnyHeader()
               .AllowAnyMethod()
